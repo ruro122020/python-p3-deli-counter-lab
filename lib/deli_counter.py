b1b73bfff7e73a4e6ec8920077336569264b65a5
Ruth <ruro122020@gmail.com>
2024-01-14 20:56:00 -0500

    now_serving function complete && lab complete

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,5 +1,3 @@
-katz_deli = []
-
 def line(queue):
   if len(queue) < 1:
     print("The line is currently empty.")
@@ -9,8 +7,13 @@ def line(queue):
       new_list.append(f"{i + 1}. {person}")
     print("The line is currently: " + ' '.join(new_list))
 
-def take_a_number():
-  pass
+def take_a_number(queue, new_person):
+  queue.append(new_person)
+  print(f"Welcome, {new_person}. You are number {len(queue)} in line.")
 
-def now_serving():
-  pass
+def now_serving(queue):
+  if len(queue) < 1:
+    print("There is nobody waiting to be served!")
+  else:
+    print(f"Currently serving {queue[0]}.")
+    del queue[0]
